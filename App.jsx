6b638ba20746e7a9801f6995e31167c4949e1fcf
nikita.prajapati <nikita.prajapati@ecosmob.com>
2026-06-12 17:56:52 +0530

    Google Login with web client ID

--- App.jsx
+++ App.jsx
@@ -1,44 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect } from 'react';
+import { View, Button, Alert, StyleSheet } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '282181382577-h8fthkqk8voa87qt10tb4f834i70qpo8.apps.googleusercontent.com',
+    });
+  }, []);
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+  const signInWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+      const signInResult = await GoogleSignin.signIn();
+
+      const idToken = signInResult.data?.idToken || signInResult.idToken;
+
+      if (!idToken) {
+        throw new Error('No ID token found');
+      }
+
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      const userCredential = await auth().signInWithCredential(
+        googleCredential,
+      );
+
+      const user = userCredential.user;
+
+      Alert.alert(
+        'Google Login Success',
+        `Name: ${user.displayName}
+Email: ${user.email}
+UID: ${user.uid}`,
+      );
+    } catch (error) {
+      console.log('Google Sign-In Error:', error);
+
+      Alert.alert('Login Failed', error.message || 'Something went wrong');
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Button title="Login with Google" onPress={signInWithGoogle} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
   },
 });
 
